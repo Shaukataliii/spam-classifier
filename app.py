39065c0fe3d25bf19015b81cc390e68eae8fbdc9
Shaukataliii <youngdevelopersk@gmail.com>
2024-07-17 16:55:20 +0500

    updated app

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from src.module import load_cache_resources
 
 st.set_page_config("Spam Classifier", page_icon="speech_balloon")
-st.title(":speech_balloon:Spam Classifier")
+st.title(":speech_balloon: Spam Classifier")
 st.caption("The model accuracy is 98.3 %, precision is 100 % and f1 score is 92.2 %")
 predictor = load_cache_resources()
 
